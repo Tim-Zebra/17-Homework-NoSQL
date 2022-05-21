abaef5694e1ce9ddefe59957b87114ab1922cb61
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-21 17:03:43 -0500

    created controller for get single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,16 +16,16 @@ module.exports = {
         return res.status(500).json(err);
       });
   },
-  // Get a single student
-  getSingleStudent(req, res) {
-    Student.findOne({ _id: req.params.studentId })
+  // Get a single user by id
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
       .select('-__v')
-      .then(async (student) =>
-        !student
-          ? res.status(404).json({ message: 'No student with that ID' })
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
           : res.json({
-              student,
-              grade: await grade(req.params.studentId),
+              user,
+              grade: await grade(req.params.userId),
             })
       )
       .catch((err) => {
